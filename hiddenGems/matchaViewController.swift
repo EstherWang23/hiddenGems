dae9ce800e7cfe208d838177e6b96ef7d644d7cf
Katharine Hong <meow24957@gmail.com>
2020-07-30 11:39:21 -0700

    message

--- hiddenGems/matchaViewController.swift
+++ hiddenGems/matchaViewController.swift
@@ -36,5 +36,8 @@ class matchaViewController: UIViewController {
     @IBAction func more(_ sender: Any) {
         imageViewer.image = UIImage(named: images[count] )
         count += 1    }
+    @IBAction func link(_ sender: Any) {
+        let url = URL (string: "https://www.google.com/maps/place/Matcha+Cafe+Maiko/@37.7426141,-122.548592,12.36z/data=!4m5!3m4!1s0x808580b8fc4e80dd:0x2d2b96f352c6134a!8m2!3d37.7849544!4d-122.4317274")!;         UIApplication.shared.open(url)
+    }
 }
 
