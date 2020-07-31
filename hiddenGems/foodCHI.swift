03ca32499fd8fb81fdefe3501e34ab539ff3d171
Kelsi <kdesimone33@gmail.com>
2020-07-30 20:46:59 -0500

    Kelsi edit 8:46

--- hiddenGems/foodCHI.swift
+++ hiddenGems/foodCHI.swift
@@ -9,7 +9,26 @@
 import UIKit
 
 class foodCHI: UIViewController {
-
+    @IBAction func wafflesButton(_ sender: Any) {
+        let url = URL (string: "https://batterandberries.com")!
+        UIApplication.shared.open(url)
+    }
+    
+    @IBAction func teamoBobaButton(_ sender: Any) {
+        let url = URL (string: "http://www.teamobobabar.com")!
+               UIApplication.shared.open(url)
+    }
+    
+    @IBAction func publicHouseButton(_ sender: Any) {
+        let url = URL (string: "https://www.publichousechicago.com")!
+        UIApplication.shared.open(url)
+    }
+    
+    @IBAction func xoMarshButton(_ sender: Any) {
+        let url = URL (string: "https://www.xomarshmallow.com")!
+        UIApplication.shared.open(url)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
--- hiddenGems/placesCHI.swift
+++ hiddenGems/placesCHI.swift
@@ -9,7 +9,23 @@
 import UIKit
 
 class placesCHI: UIViewController {
-
+    @IBAction func oliveParkButton(_ sender: Any) {
+        let url = URL (string: "https://tclf.org/landscapes/milton-lee-olive-park")!
+               UIApplication.shared.open(url)
+    }
+    
+    @IBAction func button(_ sender: Any) {
+        let url = URL (string: "https://www.the606.org")!
+        UIApplication.shared.open(url)
+    }
+    @IBAction func londonHouseButton(_ sender: Any) {
+        let url = URL (string: "https://londonhousechicago.com/rooftop/")!
+        UIApplication.shared.open(url)
+    }
+    @IBAction func mcaButton(_ sender: Any) {
+        let url = URL (string: "https://mcachicago.org")!
+        UIApplication.shared.open(url)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
